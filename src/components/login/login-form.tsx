a7d5fc26ebdbb292c3b25f77704b28ba8f0263cd
Heder Milani <hedermilani@hotmail.com>
2024-09-05 22:50:39 -0300

    ADD: Components to login page

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import styles from "./login-form.module.css";
+import stylesBtn from "@/components/form/button.module.css";
 import Login from "@/action/login";
 import { useFormStatus, useFormState } from "react-dom";
 import Button from "@/components/form/button";
 import Input from "@/components/form/input";
 import ErrorMessage from "@/components/helper/error-message";
 import React from "react";
+import Link from "next/link";
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -43,6 +45,16 @@ export default function LoginForm() {
         <ErrorMessage error={state.error} />
         <FormButton />
       </form>
+      <Link href="conta/perdeu" className={styles.lost}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.create}>
+        <h2 className={styles.subtitle}>Criar conta</h2>
+        <p>Ainda não possui uma conta? Crie uma agora mesmo!</p>
+        <Link href="conta/criar" className={stylesBtn.button}>
+          Cadastre-se
+        </Link>
+      </div>
     </>
   );
 }
