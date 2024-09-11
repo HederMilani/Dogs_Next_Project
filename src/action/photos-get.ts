a2defbe1776ebe2e65a624996aae3da88e6dd045
Heder Milani <hedermilani@hotmail.com>
2024-09-10 21:44:41 -0300

    ADD:  Page and Action to feed

--- src/action/photos-get.ts
+++ src/action/photos-get.ts
@@ -1,17 +1,37 @@
 "use server";
 
 import { Photo } from "@/types/photo";
+import errorApi from "@/functions/error-api";
+import { PHOTOS_GET } from "@/functions/api";
 
-export default async function PhotosGet() {
-  const response = await fetch(
-    "https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0",
-    {
+type PhotosGetProps = {
+  page?: number;
+  total?: number;
+  user?: 0 | string;
+};
+
+export default async function PhotosGet({
+  page = 1,
+  total = 6,
+  user = 0,
+}: PhotosGetProps = {}) {
+  try {
+    const { url } = PHOTOS_GET({ page, total, user });
+    const response = await fetch(url, {
       next: {
         revalidate: 10,
         tags: ["photos"],
       },
-    },
-  );
-  const json = (await response.json()) as Photo[];
-  return json;
+    });
+    if (!response.ok) throw new Error("Erro ao pegar as fotos");
+
+    const data = (await response.json()) as Photo[];
+    return {
+      data,
+      ok: true,
+      error: null,
+    };
+  } catch (e) {
+    return errorApi(e);
+  }
 }
--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,30 @@
+import { Metadata } from "next";
+import UserGet from "@/action/user-get";
+import PhotosGet from "@/action/photos-get";
+import Feed from "@/components/feed/feed";
+
+export const metadata: Metadata = {
+  title: "Conta | Dogs",
+  description: "Página de conta do site.",
+};
+
 export default async function ContaPage() {
+  const { data: user } = await UserGet();
+  const { data, error } = await PhotosGet({ user: user?.username });
+
   return (
-    <main>
-      <h1 className="title">Conta</h1>
-    </main>
+    <div className="animeLeft">
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            Nenhuma foto encontrada.
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
