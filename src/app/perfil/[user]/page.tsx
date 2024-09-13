98e121f6a2bffd11ea58227910d65c51e4e997c4
Heder Milani <hedermilani@hotmail.com>
2024-09-12 23:26:46 -0300

    ADD: Page profile

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,13 +1,20 @@
+import PhotosGet from "@/action/photos-get";
+import Feed from "@/components/feed/feed";
+
 type PageParams = {
   params: {
     user: string;
   };
 };
 
-export default function PerfilUserPage({ params }: PageParams) {
+export default async function PerfilUserPage({ params }: PageParams) {
+  const { data } = await PhotosGet();
+
+  if (!data) return null;
   return (
-    <main>
-      <h1 className="title">Perfil - {params.user}</h1>
-    </main>
+    <section className="container mainContainer">
+      <h1 className="title">{params.user.toLocaleUpperCase()}</h1>
+      <Feed user={params.user} photos={data} />
+    </section>
   );
 }
--- src/app/perfil/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function PerfilPage() {
-  return (
-    <main>
-      <h1 className="title">Perfil</h1>
-    </main>
-  );
-}
